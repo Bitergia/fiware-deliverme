81a824ec964876d5b8866acaf5a96dadc885a897
Alvaro del Castillo <acs@bitergia.com>
2015-03-23 11:00:55 +0100

    Added projects.json support to load data from projects available and deliverables. Start working in errors control and viz.

--- client/app/deliverables/deliverables.js
+++ client/app/deliverables/deliverables.js
@@ -52,6 +52,18 @@ angular.module('deliverMe.deliverables', ['ngRoute'])
         });
     };
 
+    $scope.load_projects = function() {
+        var url = devel_url + '/static/app/' + '/projects.json'
+        console.log(url);
+        $http.get(url).success(function(data) {
+            $scope.projects = data;
+        });
+    };
+
+    $scope.select_project = function() {
+        $scope.project_deliverables = $scope.projects[$scope.deliver.project];
+    };
+
     $scope.deliver_me = function() {
         // Create a deliverable using the project and wiki page selected
         console.log("Delivering for project " + $scope.deliver.project + " the page " + $scope.deliver.page);
@@ -60,8 +72,8 @@ angular.module('deliverMe.deliverables', ['ngRoute'])
         data += '&page='+$scope.deliver.page;
 
         // TODO: Move to a secure URL once in production
-        var url = 'http://127.0.0.1:5000/';
-        var api = "api/deliverable"
+        var url = devel_url;
+        var api = "/api/deliverable"
 
         $http({method:'GET',url:url+api+"?"+data})
         .success(function(data, status, headers, config){
@@ -70,11 +82,14 @@ angular.module('deliverMe.deliverables', ['ngRoute'])
         error(function(data,status,headers,config){
           console.log("Probs generating deliverable  " + data);
           $scope.error = data;
+          $scope.deliver_result="error";
         });
     };
     // https://github.com/angular-ui/bootstrap/wiki/FAQ#my-input--select-element-doesnt-work-as-expected-inside-the-tab--accordion-directive
     $scope.deliver = {"project":"","page":""};
+    $scope.error = undefined;
     $scope.load_deliverables();
+    $scope.load_projects();
     $scope.dash_selected = {name:""};
-    $scope.projects = ["fiware","fi-ware-review","testbed","apps","cloud","data","i2nd","iot","security","miwi","tools","exploitation","fi-ware-private"];
+    // $scope.projects = ["fiware","fi-ware-review","testbed","apps","cloud","data","i2nd","iot","security","miwi","tools","exploitation","fi-ware-private"];
 }]);
