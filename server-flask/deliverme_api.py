ed072d8d960006124ab63a6168590e814bbf88d0
Alvaro del Castillo <acs@bitergia.com>
2015-03-26 18:29:46 +0100

    Added progress bar and URL for WP generated.

--- server-flask/deliverme_api.py
+++ server-flask/deliverme_api.py
@@ -47,7 +47,7 @@ def check_login(user, passwd):
 
 def generate_deliverable(project, page):
     deliverme_dir = "/home/acs/devel/fiware-deliverme"
-    deliverme_dir = "/home/lcanas/repos/fiware-deliverme"
+    # deliverme_dir = "/home/lcanas/repos/fiware-deliverme"
     # Move to apache later
     deliverables_dir = deliverme_dir + "/server-flask/static/deliverables"
     deliverables_dir = "/var/www/deliverme/deliverables"
