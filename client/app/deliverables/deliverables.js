856e6642402f9ba7c5441fd869d04768e6a97bad
Luis Cañas Díaz <lcanas@bitergia.com>
2015-09-25 19:52:03 +0200

    Help text improved for the generation form

--- client/app/deliverables/deliverables.js
+++ client/app/deliverables/deliverables.js
@@ -33,6 +33,54 @@ angular.module('deliverMe.deliverables', ['ngRoute'])
     });
 })
 
+.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+})
+.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+})
+
 .factory('webtest', function($q, $timeout, $http) {
     var Webtest = {
         fetch: function(callback) {
@@ -232,7 +280,15 @@ angular.module('deliverMe.deliverables', ['ngRoute'])
 
 
 }]);
-
+/*
+angular.module('ui.bootstrap.demo').controller('PopoverDemoCtrl', function ($scope) {
+  $scope.dynamicPopover = {
+    content: 'Hello, World!',
+    templateUrl: 'myPopoverTemplate.html',
+    title: 'Title'
+  };
+});
+*/
 /**
  * Return a timestamp with the format "m/d/yy h:MM:ss TT"
  * @type {Date}
